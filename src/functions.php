c1f1f1ce3393162cbd58a135b14ca766d5549f9c
Reinier Ladan <reinierl@gmail.com>
2015-03-20 14:49:31 +0100

    Better support for post formats

--- src/functions.php
+++ src/functions.php
@@ -8,9 +8,6 @@
 
   add_action( 'init', 'theme_features' );
 
-
-// --------------------------------------- 
-
   function ganymede_customize_register($wp_customize){
     
     $wp_customize->add_section('ganymede_options', array(
@@ -19,20 +16,10 @@
         'priority' => 120,
     ));
  
-    //  =============================
-    //  = Checkbox                  =
-    //  =============================
     $wp_customize->add_setting('ganymede_options_tagline', array(
         'capability' => 'edit_theme_options',
         'default'    => '1'
     ));
- 
-    // $wp_customize->add_control('display_header_text', array(
-    //     'settings' => 'ganymede_theme_options[checkbox_tagline]',
-    //     'label'    => __('Display tagline'),
-    //     'section'  => 'ganymede_options',
-    //     'type'     => 'checkbox',
-    // ));
 
     $wp_customize->add_control(
       new WP_Customize_Control(
@@ -46,26 +33,28 @@
         )
       )
     );
+  }
  
-    //  =============================
-    //  = Color Picker              =
-    //  =============================
-    // $wp_customize->add_setting('ganymede_theme_options[link_color]', array(
-    //     'default'           => '000',
-    //     'sanitize_callback' => 'sanitize_hex_color',
-    //     'capability'        => 'edit_theme_options',
-    //     'type'           => 'option',
- 
-    // ));
- 
-    // $wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'link_color', array(
-    //     'label'    => __('Link Color', 'ganymede'),
-    //     'section'  => 'ganymede_options',
-    //     'settings' => 'ganymede_theme_options[link_color]',
-    // )));
+  add_action('customize_register', 'ganymede_customize_register');
 
-}
- 
-add_action('customize_register', 'ganymede_customize_register');
+  // --------------------------------------- 
+  /* Common purpose functions */
+  // --------------------------------------- 
+
+  function addVideoWrapperToVideos($string) {
+    // match any iframes
+    $pattern = '~<iframe.*?</iframe>~';
+    $content = preg_replace_callback($pattern, function($matches){
+      if(strpos($matches[0], 'youtube') !== false OR strpos($matches[0], 'vimeo') !== false){
+        return '<div class="videoWrapper">' . $matches[0] . '</div>';
+      }
+
+      return $matches[0];
+    }, $string);
+
+    return $content;
+  }
+
+  add_filter( 'the_content', 'addVideoWrapperToVideos' );
 
 ?>
