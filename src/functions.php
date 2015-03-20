c2d9fef464988fb5ac2e314d35da77cded4309fd
Reinier Ladan <reinierl@gmail.com>
2015-03-20 11:20:01 +0100

    Design update

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,71 @@
 <?php
+  
+  function theme_features () {
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-formats', array( 'aside', 'link', 'video' ) );
+    register_nav_menu('header-nav',__( 'Header navigation' ));
+  }
 
-  add_theme_support( 'post-thumbnails' );
-  add_theme_support( 'post-formats', array( 'aside', 'link', 'video' ) );
+  add_action( 'init', 'theme_features' );
+
+
+// --------------------------------------- 
+
+  function ganymede_customize_register($wp_customize){
+    
+    $wp_customize->add_section('ganymede_options', array(
+        'title'    => __('Options', 'ganymede'),
+        'description' => '',
+        'priority' => 120,
+    ));
+ 
+    //  =============================
+    //  = Checkbox                  =
+    //  =============================
+    $wp_customize->add_setting('ganymede_options_tagline', array(
+        'capability' => 'edit_theme_options',
+        'default'    => '1'
+    ));
+ 
+    // $wp_customize->add_control('display_header_text', array(
+    //     'settings' => 'ganymede_theme_options[checkbox_tagline]',
+    //     'label'    => __('Display tagline'),
+    //     'section'  => 'ganymede_options',
+    //     'type'     => 'checkbox',
+    // ));
+
+    $wp_customize->add_control(
+      new WP_Customize_Control(
+        $wp_customize,
+        'ganymede_options_tagline',
+        array(
+          'label'     => __('Display tagline'),
+          'section'   => 'ganymede_options',
+          'settings'  => 'ganymede_options_tagline',
+          'type'      => 'checkbox',
+        )
+      )
+    );
+ 
+    //  =============================
+    //  = Color Picker              =
+    //  =============================
+    // $wp_customize->add_setting('ganymede_theme_options[link_color]', array(
+    //     'default'           => '000',
+    //     'sanitize_callback' => 'sanitize_hex_color',
+    //     'capability'        => 'edit_theme_options',
+    //     'type'           => 'option',
+ 
+    // ));
+ 
+    // $wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'link_color', array(
+    //     'label'    => __('Link Color', 'ganymede'),
+    //     'section'  => 'ganymede_options',
+    //     'settings' => 'ganymede_theme_options[link_color]',
+    // )));
+
+}
+ 
+add_action('customize_register', 'ganymede_customize_register');
 
 ?>
--- src/header.php
+++ src/header.php
@@ -28,6 +28,21 @@
 
 <body <?php body_class(); ?>>
   <div class="page-root">
-    <header>
-      <h1 id="logo"><a href="<?php echo get_option('home'); ?>/"><span><?php bloginfo('name'); ?></span></a></h1>
+    <?php
+      $show_tagline = get_theme_mod('ganymede_options_tagline',1);
+      $tagline = get_bloginfo('description');
+    ?>
+    <header <?php if($show_tagline != 1 || $tagline == ''){ echo 'class="no-tagline"'; }?>>
+      <h1 class="logo"><a href="<?php echo get_option('home'); ?>/"><span><?php bloginfo('name'); ?></span></a></h1>
+      <?php if($show_tagline == 1 && $tagline != ''){ ?>
+        <p class="tagline"><?php echo $tagline; ?></p>
+      <?php } ?>
+      <nav>
+        <?php
+          $settings = array(
+            'theme_location' => 'header-nav'
+          );
+          wp_nav_menu($settings);
+        ?>
+      </nav>
     </header>
